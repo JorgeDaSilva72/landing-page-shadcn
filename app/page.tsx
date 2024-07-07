e9337885c03ae9c8aab6df9c4bc5b1120e07a087
JorgeDaSilva72 <73615973+JorgeDaSilva72@users.noreply.github.com>
2024-07-07 13:01:09 +0200

    Creating SponsorsSection.

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,12 @@
 import { HeroSection } from "@/components/layout/sections/hero";
 import { FooterSection } from "@/components/layout/sections/footer";
+import { SponsorsSection } from "@/components/layout/sections/sponsors";
 
 export default function Home() {
   return (
     <>
       <HeroSection />
+      <SponsorsSection />
       <FooterSection />
     </>
   );
---
+++ components/layout/sections/sponsors.tsx
@@ -0,0 +1,75 @@
+"use client";
+
+import { Icon } from "@/components/ui/icon";
+import { Marquee } from "@devnomic/marquee";
+import "@devnomic/marquee/dist/index.css";
+import { icons } from "lucide-react";
+interface sponsorsProps {
+  icon: string;
+  name: string;
+}
+
+const sponsors: sponsorsProps[] = [
+  {
+    icon: "Crown",
+    name: "World Bank",
+  },
+  {
+    icon: "Vegan",
+    name: "FMI",
+  },
+  {
+    icon: "Ghost",
+    name: "UE",
+  },
+  {
+    icon: "Puzzle",
+    name: "Google ",
+  },
+  {
+    icon: "Squirrel",
+    name: "Microsoft ",
+  },
+  {
+    icon: "Cookie",
+    name: "IBM ",
+  },
+  {
+    icon: "Drama",
+    name: "Coca-Cola Africa Foundation",
+  },
+];
+
+export const SponsorsSection = () => {
+  return (
+    <section id="sponsors" className="max-w-[75%] mx-auto pb-24 sm:pb-32">
+      <h2 className="text-lg md:text-xl text-center mb-6">NOS SPONSORS</h2>
+
+      <div className="mx-auto">
+        <Marquee
+          className="gap-[3rem] [--duration:8s]"
+          fade={true}
+          direction="left"
+          reverse={false}
+          pauseOnHover={true}
+          innerClassName="gap-[3rem]"
+        >
+          {sponsors.map(({ icon, name }) => (
+            <div
+              key={name}
+              className="flex items-center text-xl md:text-2xl font-medium"
+            >
+              <Icon
+                name={icon as keyof typeof icons}
+                size={32}
+                color="#D14F0C"
+                className="mr-2"
+              />
+              {name}
+            </div>
+          ))}
+        </Marquee>
+      </div>
+    </section>
+  );
+};
