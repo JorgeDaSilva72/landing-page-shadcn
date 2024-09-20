8d544c00bc63ae603af2b31e50cf01ee3fd4c2b3
JorgeDaSilva72 <73615973+JorgeDaSilva72@users.noreply.github.com>
2024-09-20 18:53:32 +0200

    Intégration de leaflet

--- components/layout/sections/hero.tsx
+++ components/layout/sections/hero.tsx
@@ -1,4 +1,5 @@
 "use client";
+import AfriqueMap from "@/components/ui/AfriqueMap";
 import { Badge } from "@/components/ui/badge";
 import { Button } from "@/components/ui/button";
 import { ArrowRight } from "lucide-react";
@@ -42,7 +43,7 @@ export const HeroSection = () => {
           </div>
         </div>
 
-        <div className="relative group mt-14">
+        {/* <div className="relative group mt-14">
           <div className="absolute top-2 lg:-top-8 left-1/2 transform -translate-x-1/2 w-[90%] mx-auto h-24 lg:h-80 bg-primary/50 rounded-full blur-3xl"></div>
           <Image
             width={1200}
@@ -57,6 +58,13 @@ export const HeroSection = () => {
           />
 
           <div className="absolute bottom-0 left-0 w-full h-20 md:h-28 bg-gradient-to-b from-background/0 via-background/50 to-background rounded-lg"></div>
+
+         
+        </div> */}
+
+        {/* <div className="max-w-screen-md  h-[600px] md:w-[1200px] mx-auto rounded-lg relative rouded-lg leading-none flex items-center border border-t-2 border-secondary  border-t-primary/30"> */}
+        <div className="max-w-screen-md  h-[600px] md:w-[1200px] mx-auto rounded-lg relative rouded-lg leading-none flex items-center">
+          <AfriqueMap />
         </div>
       </div>
     </section>
---
+++ components/ui/AfriqueMap.tsx
@@ -0,0 +1,47 @@
+import React from "react";
+import { MapContainer, TileLayer, Polygon, Popup } from "react-leaflet";
+import "leaflet/dist/leaflet.css";
+import L from "leaflet";
+
+// Nécessaire pour afficher les marqueurs correctement
+// delete L.Icon.Default.prototype._getIconUrl;
+L.Icon.Default.mergeOptions({
+  iconRetinaUrl: require("leaflet/dist/images/marker-icon-2x.png"),
+  iconUrl: require("leaflet/dist/images/marker-icon.png"),
+  shadowUrl: require("leaflet/dist/images/marker-shadow.png"),
+});
+
+// Définissez un type pour les coordonnées
+type LatLngTuple = [number, number];
+
+const AfriqueMap: React.FC = () => {
+  // Coordonnées pour le Sénégal
+  //   const senegal: LatLngTuple[] = [
+  //     [14.0, -17.0],
+  //     [14.0, -15.0],
+  //     [12.0, -15.0],
+  //     [12.0, -17.0],
+  //   ];
+  return (
+    <div className=" mx-auto ">
+      {/* <h1>Carte Interactive de l'Afrique</h1> */}
+      <MapContainer
+        center={[1.5, 17]}
+        zoom={3}
+        style={{ height: "600px", width: "400px" }}
+      >
+        <TileLayer
+          url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+          maxZoom={19}
+          attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+        />
+        {/* <Polygon positions={senegal}>
+          <Popup>Sénégal</Popup>
+        </Polygon> */}
+        {/* Ajoutez d'autres polygones pour d'autres pays ici */}
+      </MapContainer>
+    </div>
+  );
+};
+
+export default AfriqueMap;
