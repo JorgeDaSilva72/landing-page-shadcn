0bbd3c80037990aadee6cfd21f42e54cf24537bc
JorgeDaSilva72 <73615973+JorgeDaSilva72@users.noreply.github.com>
2024-09-22 11:59:27 +0200

    Le bouton BackToTop n'apparait qu après un scroll de la souris .

--- components/BackToTop.jsx
+++ components/BackToTop.jsx
@@ -1,25 +1,43 @@
 "use client";
+import { useState, useEffect } from "react";
 
 import { useTheme } from "next-themes";
 
 const BackToTop = () => {
+  const [showTopBtn, setShowTopBtn] = useState(false);
+
+  useEffect(() => {
+    window.addEventListener("scroll", () => {
+      if (window.scrollY > 400) {
+        setShowTopBtn(true);
+      } else {
+        setShowTopBtn(false);
+      }
+    });
+  }, []);
+
   const { theme } = useTheme();
   const scrollToTop = () => {
     window.scrollTo({
       top: 0,
+      left: 0,
       behavior: "smooth",
     });
   };
 
   return (
-    <button
-      onClick={scrollToTop}
-      className={`fixed bottom-4 right-4 w-8 h-8 md:w-12 md:h-12 bg-orange-500 ${
-        theme === "light" ? "text-white" : "text-white"
-      } rounded-full flex items-center justify-center shadow-lg hover:bg-orange-600 transition duration-300`}
-    >
-      ↑
-    </button>
+    <>
+      {showTopBtn && (
+        <button
+          onClick={scrollToTop}
+          className={`fixed bottom-4 right-4 w-8 h-8 md:w-12 md:h-12 bg-orange-500 ${
+            theme === "light" ? "text-white" : "text-white"
+          } rounded-full flex items-center justify-center shadow-lg hover:bg-orange-600 transition duration-300`}
+        >
+          ↑
+        </button>
+      )}
+    </>
   );
 };
 
