de14403b41e52501f59812309f726b817e050a07
JorgeDaSilva72 <73615973+JorgeDaSilva72@users.noreply.github.com>
2025-01-14 19:16:19 +0100

    Ajout de panneaux publicitaires (lws,quonto,revolut, goophy)

--- lib/data.ts
+++ lib/data.ts
@@ -1,19 +1,26 @@
 export const ads = [
   {
-    imageUrl: "/pub.png",
-    linkUrl: "#contact",
-    altText: "Annonce 1",
+    imageUrl: "/lws.png",
+    linkUrl: "https://www.lws.fr/",
+    altText: "lws",
   },
   {
-    imageUrl: "/pub.png",
-    linkUrl: "#contact",
-    altText: "Annonce 1",
+    imageUrl: "/quonto.png",
+    linkUrl: "https://qonto.com",
+    altText: "quonto",
+  },
+
+  {
+    imageUrl: "/goopy_368x63.jpg",
+    linkUrl: "https://www.goopy.fr/",
+    altText: "goopy",
   },
   {
-    imageUrl: "/pub.png",
-    linkUrl: "#contact",
-    altText: "Annonce 1",
+    imageUrl: "/320px-Logo_Revolut.png",
+    linkUrl: "https://www.revolut.com",
+    altText: "revolut",
   },
+
   {
     imageUrl: "/pub.png",
     linkUrl: "#contact",
